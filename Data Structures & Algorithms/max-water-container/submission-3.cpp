7b303c78007ac5fa5164ade6caa0804c0fd70af7
Dalton Harrold <djharrold1@gmail.com>
2026-07-28 14:13:40 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+    int maxArea(vector<int>& heights) {
+        // Guard against 0 and 1 length vectors
+        if (heights.size() < 2) {
+            return 0;
+        }
+        // Set 'pointers'
+        int l = 0;
+        int r = heights.size() - 1;
+
+        // Current water fill max
+        int max_vol = 0;
+
+        // Iterate using two pointer alg
+        while (l < r) {
+            const int min_height = heights[l] < heights[r] ? heights[l] : heights[r];
+            const int this_vol = min_height * (r - l);
+            
+            // New largest water container found!
+            if (this_vol > max_vol) {
+                max_vol = this_vol;
+            }
+            
+            // Move shorter bar's pointer inwards
+            if (heights[l] < heights[r]) {
+                ++l;
+            } else {
+                --r;
+            }
+        }
+        return max_vol;
+    }
+};
